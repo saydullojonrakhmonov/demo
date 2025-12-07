14dc988de14ea0ba2a27ed4c8739708abdcd1efb
rahmonovsaydullo <saydullo779@gmail.com>
2025-12-07 15:22:30 +0500

    Reset base path to root

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/demo/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
